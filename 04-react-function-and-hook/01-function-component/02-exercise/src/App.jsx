da6d854bf04359d3156e6f19810e446fb27c88c7
Wipoo Suksumek <124798841+wipoosuk@users.noreply.github.com>
2023-04-10 10:09:24 +0700

    first commit

--- 04-react-function-and-hook/01-function-component/02-exercise/src/App.jsx
+++ 04-react-function-and-hook/01-function-component/02-exercise/src/App.jsx
@@ -12,29 +12,36 @@ function App() {
 
 function Header() {
   return (
-    // Code here
-    // <Header />
+    <header>
+    <span>Turn on / off</span>
+    <p>Current Temperature: XXX</p>
+    </header>
   );
 }
 
 function Content() {
   return (
-    // Code here
-    // <Content />
+    <main>
+    <Temperature />
+    </main>
   );
 }
 
 function Temperature() {
+  let temperature = 30
   return (
-    // Code here
-    // <Temperature />
+    <div id="temperature">
+    <span> {temperature} Oc</span>
+    </div>
   );
 }
 
 function Footer() {
   return (
-    // Code here
-    // <Footer />
+    <footer>
+    <button>Up</button>
+    <button>Down</button>
+    </footer>
   );
 }
 
